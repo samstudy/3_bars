c633274b9d23ebacf23d185dd8c23f628120d551
Samat Karymshakov <bskordo@gmail.com>
2016-12-15 23:34:12 +0600

    Task number 3 done

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # 3_bars
+
+###The script will find the biggest and smallest bar according json file which downloaded from http://data.mos.ru/
+### Run pip install -r requirements.txt to install geopy which makes easy for Python developers to locate the coordinates of addresses, cities, countries, and landmarks across the globe using third-party geocoders and other data sources.
+- Need to input into the script the **folder's path where located json file**
+- Run the script
+- Sript will required your coordinate:latitude and longitude to find the closest bar
--- bars.py
+++ bars.py
@@ -1,21 +1,49 @@
 import json
+import os
+from geopy.distance import vincenty
+
+
+my_location = []
+my_langitute = float(input('Please input latitude:'))
+my_lattitute = float(input('Please input longitude:'))
+my_location.append([my_langitute, my_lattitute])
 
 
 def load_data(filepath):
-    pass
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, 'r') as file_handler:
+        return json.load(file_handler)
+
+
+def get_seats_count(bar):
+    return bar['Cells']['SeatsCount']
+
+
+def get_distance(bar):
+    bars_coordinate = bar['Cells']['geoData']['coordinates']
+    distance = vincenty(bars_coordinate, my_location).miles
+    return distance
 
 
 def get_biggest_bar(data):
-    pass
+    biggest_bar = max(data, key=get_seats_count)
+    return biggest_bar['Cells']['Name']
 
 
 def get_smallest_bar(data):
-    pass
+    smallest_bar = min(data, key=get_seats_count)
+    return smallest_bar['Cells']['Name']
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(data):
+    closest_bar = min(data, key=get_distance)
+    return closest_bar['Cells']['Name']
+
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data('put_a_path')
+    print('The biggest bar:', get_biggest_bar(data))
+    print('The smallest bar:', get_smallest_bar(data))
+    print('The closest bar:', get_closest_bar(data))
